6857d9cf18787b52aa1e7bdd3ad221c90b780222
Den4iks <lol4ik99@gmail.com>
2016-04-07 02:29:57 +0300

    test

---
+++ src/main/java/Server.java
@@ -0,0 +1,179 @@
+import com.google.gson.Gson;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Starter;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeEvent;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+
+import java.io.IOException;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+/**
+ * Created by den4ik on 05-Apr-16.
+ */
+public class Server extends AbstractVerticle {
+
+    private Logger logger = LoggerFactory.getLogger(Server.class);
+    private SockJSHandler handler = null;
+    private AtomicInteger online = new AtomicInteger(0);
+
+    public void start() throws Exception {
+
+        if(!deploy()){
+            logger.error("Failed to deploy the server");
+            return;
+        }
+        handle();
+    }
+
+    private boolean deploy (){
+        int hostPort = getFreePort();
+
+        if (hostPort<0) return false;
+
+        Router router = Router.router(vertx);
+
+        handler = SockJSHandler.create(vertx);
+
+        router.route("/eventbus").handler(handler);
+        router.route().handler(StaticHandler.create());
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(hostPort);
+
+        try{
+            String addr = InetAddress.getLocalHost().getHostAddress();
+            logger.info("Access to CHAT is available by the following address http://" + addr + ":" + hostPort);
+        }catch (UnknownHostException e){
+            logger.error("Failed to get the localhost address: " + e.toString());
+            return false;
+        }
+        return true;
+    }
+
+    private int getFreePort(){
+        int hostPort = 8080;
+
+        if(Starter.PROCESS_ARGS != null && Starter.PROCESS_ARGS.size() > 0){
+            try{
+                hostPort = Integer.valueOf(Starter.PROCESS_ARGS.get(0));
+            }catch (NumberFormatException e ){
+                logger.warn("Invalid port: " + Starter.PROCESS_ARGS.get(0));
+            }
+        }
+        if (hostPort < 0 || hostPort > 65535){
+            hostPort = 8080;
+        }
+        return getFreePort(hostPort);
+    }
+
+    private int getFreePort(int hostPort){
+        try{
+            ServerSocket socket = new ServerSocket(hostPort);
+            int port = socket.getLocalPort();
+            socket.close();
+            return port;
+        }catch (BindException e){
+            if(hostPort != 0) return getFreePort(0);
+            logger.error("Failed to get the free port: " + e.toString());
+            return -1;
+        }catch (IOException e){
+            logger.error("Failed to get the free port: " + e.toString());
+            return -1;
+        }
+    }
+
+    private void handle (){
+        BridgeOptions opts = new BridgeOptions()
+                .addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"))
+                .addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client"));
+
+        handler.bridge(opts, event ->{
+            if(event.type() == BridgeEvent.Type.PUBLISH)
+                publishEvent(event);
+
+            if(event.type() == BridgeEvent.Type.REGISTER)
+                registerEvent(event);
+
+            if(event.type() == BridgeEvent.Type.SOCKET_CLOSED)
+                closeEvent(event);
+
+            event.complete();
+        });
+    }
+
+    private boolean publishEvent(BridgeEvent event){
+        if(event.rawMessage() != null && event.rawMessage().getString("address").equals("chat.to.server")){
+            String message = event.rawMessage().getString("body");
+            if(!verifyMessage(message))
+                return false;
+
+            String host = event.socket().remoteAddress().host();
+            int port = event.socket().remoteAddress().port();
+
+            Map<String, Object> publicNotice = createPublicNotice(host, port, message);
+                vertx.eventBus().publish("chat.to.client", new Gson().toJson(publicNotice));
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+    private Map<String, Object> createPublicNotice(String host, int port, String message){
+        Date time = Calendar.getInstance().getTime();
+
+        Map<String, Object> notice = new TreeMap<>();
+        notice.put("type", "publish");
+        notice.put("time", time.toString());
+        notice.put("host", host);
+        notice.put("port", port);
+        notice.put("message", message);
+        return notice;
+    }
+
+    private void registerEvent(BridgeEvent event){
+        if(event.rawMessage() !=null && event.rawMessage().getString("address").equals("chat.to.client"))
+            new Thread(() ->
+            {Map<String, Object> registerNotice = createRegisterNotice();
+                vertx.eventBus().publish("chat.to.client",new Gson().toJson(registerNotice));
+            }
+            ).start();
+    }
+
+    private Map<String, Object> createRegisterNotice(){
+        Map<String, Object> notice = new TreeMap<>();
+        notice.put("type", "register");
+        notice.put("online", online.incrementAndGet());
+        return notice;
+    }
+
+    private void closeEvent(BridgeEvent event){
+        new Thread(() ->
+        {
+            Map<String, Object> closeNotice = createCloseNotice();
+            vertx.eventBus().publish("client.to.server",new Gson().toJson(closeNotice));
+        }).start();
+    }
+
+    private Map<String, Object> createCloseNotice(){
+        Map<String, Object> notice = new TreeMap<>();
+        notice.put("type", "close");
+        notice.put("online", online.decrementAndGet());
+        return notice;
+    }
+
+    private boolean verifyMessage(String message){
+        return message.length() > 0 && message.length() <= 140;
+    }
+
+}
---
+++ src/main/java/VerticLoader.java
@@ -0,0 +1,71 @@
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+/**
+ * Created by den4ik on 07-Apr-16.
+ */
+public class VerticLoader {
+        private static Vertx vertx;
+
+        public static Vertx getVertx() {
+            return vertx;
+        }
+
+        public static void load() {
+            load(null);
+        }
+
+        public static void load(Handler<AsyncResult<String>> completionHandler) {
+            VertxOptions options = new VertxOptions().setClustered(false);
+            //путь до verticle-класса.
+            String dir = "chat/src/main/java/";
+
+            try {
+                File current = new File(".").getCanonicalFile();
+                if (dir.startsWith(current.getName()) && !dir.equals(current.getName())) {
+                    dir = dir.substring(current.getName().length() + 1);
+                }
+            } catch (IOException e) {
+            }
+
+            System.setProperty("vertx.cwd", dir);
+            String verticleID = Server.class.getName();
+
+            Consumer<Vertx> runner = vertx ->
+            {
+                try {
+                    if (completionHandler == null)
+                        vertx.deployVerticle(verticleID);
+                    else
+                        vertx.deployVerticle(verticleID, completionHandler);
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+            };
+
+            if (options.isClustered()) {
+                Vertx.clusteredVertx(options, res ->
+                {
+                    if (res.succeeded()) {
+                        vertx = res.result();
+                        runner.accept(vertx);
+                    } else {
+                        res.cause().printStackTrace();
+                    }
+                });
+            } else {
+                vertx = Vertx.vertx(options);
+                runner.accept(vertx);
+            }
+        }
+
+        public static void main(String[] args) {
+            load();
+        }
+}
